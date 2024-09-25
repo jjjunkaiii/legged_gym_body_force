441b17d644e7693d76d75d629dd211afa04f20dd
marmot <junkai.lu.chn@gmail.com>
2024-09-25 20:44:12 +0800

    fork from legged_gym and add anymal_c_flat/rough envs

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -34,6 +34,10 @@ from .base.legged_robot import LeggedRobot
 from .anymal_c.anymal import Anymal
 from .anymal_c.mixed_terrains.anymal_c_rough_config import AnymalCRoughCfg, AnymalCRoughCfgPPO
 from .anymal_c.flat.anymal_c_flat_config import AnymalCFlatCfg, AnymalCFlatCfgPPO
+
+from .anymal_c_force.mixed_terrains.anymal_c_rough_config import AnymalCRoughForceCfg, AnymalCRoughForceCfgPPO
+from .anymal_c_force.flat.anymal_c_flat_config import AnymalCFlatForceCfg, AnymalCFlatForceCfgPPO
+
 from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
 from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
@@ -46,6 +50,8 @@ from legged_gym.utils.task_registry import task_registry
 
 task_registry.register( "anymal_c_rough", Anymal, AnymalCRoughCfg(), AnymalCRoughCfgPPO() )
 task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCfgPPO() )
+task_registry.register( "anymal_c_rough_force", Anymal, AnymalCRoughForceCfg(), AnymalCRoughForceCfgPPO() )
+task_registry.register( "anymal_c_flat_force", Anymal, AnymalCFlatForceCfg(), AnymalCFlatForceCfgPPO() )
 task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
